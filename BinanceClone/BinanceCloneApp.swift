1ce319434d96048c679dd3deff9ad51f60ca8950
pagh2322 <pagh2322@naver.com>
2022-06-07 21:16:26 +0900

    first commit

---
+++ BinanceClone/BinanceCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  BinanceCloneApp.swift
+//  BinanceClone
+//
+//  Created by peo on 2022/06/07.
+//
+
+import SwiftUI
+
+@main
+struct BinanceCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
